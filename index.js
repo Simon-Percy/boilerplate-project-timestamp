cab9c0bbb1430dd20cf0efcfd05ee1a563b40a30
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-21 11:26:37 +0200

    account for unxi in params

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/1451001600000", (req, res) => {
-  const uni = Number(1451001600000);
-  res.json({ unix: uni, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
-});
-app.get("/api/:date", (req, res) => {
+app.get("/api/:date?", (req, res) => {
   let date = req.params.date;
   if (req.params.date === undefined) {
     date = new Date().toUTCString();
+  } else if (date.match(/^\d+$/g)) {
+    const uni = Number(req.params.date);
+    date = new Date(uni).toUTCString();
+
+    res.json({ unix: uni, utc: date });
+    return;
   } else if (!date.match(/^(\d{4})-(\d{2})-(\d{2})$/)) {
     res.json({ error: "Invalid Date" });
     return;
@@ -33,10 +35,6 @@ app.get("/api/:date", (req, res) => {
   const longDate = new Date(date).toUTCString();
   const uni = Number(Date.parse(date));
   res.json({ unix: uni, utc: longDate });
-});
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
 });
 
 // listen for requests :)
