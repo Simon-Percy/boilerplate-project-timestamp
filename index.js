8622bd76951e898e9d7e0d6e32b83cf51ee6aefd
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-21 08:21:05 +0200

    project seems finished

--- index.js
+++ index.js
@@ -18,12 +18,20 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/:date?", (req, res) => {
-  const date = req.params.date;
-  res.json({ unix: date, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
-});
 app.get("/api/1451001600000", (req, res) => {
-  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+  const uni = Number(1451001600000);
+  res.json({ unix: uni, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
+  if (req.params.date === undefined) {
+    date = new Date().toString();
+  } else if (!date.match(/(\d-\d+-\d)/g)) {
+    res.json({ error: "Invalid Date" });
+  }
+  const longDate = new Date(date).toString();
+  const uni = Number(Date.parse(date));
+  res.json({ unix: uni, utc: longDate });
 });
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
